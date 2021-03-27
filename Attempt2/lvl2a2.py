1d5dd1252a4729b7331edc59b54990b297a3f0c2
Logan Montgomery <lmmontgo21@wabash.edu>
2021-03-27 12:23:51 -0400

    added lvl2

--- Attempt2/lvl2a2.py
+++ Attempt2/lvl2a2.py
@@ -1,41 +1,13 @@
-# I dont remember the eaxt problem statment,
-# solution() does stu with the sum if fib numbers
-###LEVEL 2.2#### 
-def f(n):
-    a, b = 0, 1
-    for i in range(0, n):
-        a, b = b, a + b
-    return a
-def sumFib(x):
-    Sum = 0
-    for i in range(x+2):
-        Sum += f(i)
-    return Sum    
-def solution(total_lambs):
-    lst = []
-    i = 0
-    while True:
-        lst.append(sumFib(i))
-        if lst[i] > total_lambs:
-            break 
-        i += 1
-    Max = len(lst)-1
-    print(lst)
-    
-    lst2 = []
-    i = 0
-    while True:
-        addLst = [1]
-        for j in range(0,i):
-            addLst.append(addLst[j]*2)
-        lst2.append(sum(addLst))
-        if lst2[i] >= total_lambs:
-            break
-        i += 1
-    Min = len(lst2)-1
-    print(lst2)
-    return(Max-Min)
-        
-#print(sumFib(5))
-print(solution(10))
-#([[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]])
+###LEVEL 2### 
+# this was the salute problem,
+# super easy people '<' walk twards eachother have to salute,
+# count the number of salutes needed in a given path
+# example --<---<--->--<--
+# return 2 salutes, bc 2 people salute 1 time each
+
+def solution(s):
+    for i in range(0,len(s)):
+        if s[i] == '>':
+            for j in range(0,len(s)-i):
+                if s[j] == '<':
+                    count += 1
