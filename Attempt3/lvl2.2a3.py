b4c37a3ce537bc47c9acbc7d8f5f9fa6425d4a20
Logan Montgomery <lmmontgo21@wabash.edu>
2021-04-01 18:00:58 -0400

    solution2

--- Attempt3/lvl2.2a3.py
+++ Attempt3/lvl2.2a3.py
@@ -3,7 +3,7 @@ def solution(pegs):
     dist = (pegs[-1])-(pegs[0])
     #print(dist)
     l1 = pegs[1]-pegs[0]
-    for m in range(1,l1-1,1):
+    for m in range(1,l1+1):
         a = dist-m
         if (l1-m)/2+l1 == a:
             val = l1-m
@@ -15,6 +15,27 @@ def solution(pegs):
             #s2 = str(f)
             #return s2       
     return[-1,-1]
+
+def solution2(pegs):
+    lst = []
+    for i in range(1,len(pegs)):
+        val = pegs[i-1]-pegs[i]
+        if val < 1:
+            val = val*-1
+        lst.append(val)
+    print(lst)
+    if len(lst) > 1:
+        solution2(lst)
+        return lst
+    else:
+        return lst
     
-print(solution([4,30,50]))
-print(solution([1,16,37]))
+        
+        
+    
+
+    
+print(solution2([4,30,50]))    
+#print(solution([4,30,50]))
+#print(solution([18,25,46]))
+#print(frac(12.34))
