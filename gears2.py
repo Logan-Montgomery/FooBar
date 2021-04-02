cd6fda743b753ca89f4242667b776568c41c4b1e
Michael High <74934387+michael-high@users.noreply.github.com>
2021-04-01 20:39:00 -0400

    Update gears2.py

--- gears2.py
+++ gears2.py
@@ -2,16 +2,20 @@ def solution(pegs):
     Ls=[]
     for i in range(1,len(pegs)):
         Ls.append(pegs[i]-pegs[i-1])
-    print(Ls)
+    #print(Ls)
     rn=Ls[0]
     for i in range(1,len(pegs)-1):
         if i%2==0:
             rn=rn+Ls[i]
         else:
             rn=rn-Ls[i]
-    print(rn)
+    #print(rn)
+    if rn<0:
+        return [-1,-1]
     if len(Ls)%2==0:
-        out=[2*rn,1]
+        x=2*rn
     else:
-        out=[(2*rn)/3.0,1]
+        x=(2*rn)/3
+    x=rn.as_integer_ratio()
+    out=[x[0],x[1]]
     return out
