d6010e6879ab82e46f8cc26ba8e703a2af274f04
Michael High <74934387+michael-high@users.noreply.github.com>
2021-04-01 21:29:57 -0400

    Update gears2.py

--- gears2.py
+++ gears2.py
@@ -1,4 +1,4 @@
-def solution(pegs):
+ef solution(pegs):
     Ls=[]
     for i in range(1,len(pegs)):
         Ls.append(pegs[i]-pegs[i-1])
@@ -10,12 +10,32 @@ def solution(pegs):
         else:
             rn=rn-Ls[i]
     #print(rn)
-    if rn<0:
-        return [-1,-1]
     if len(Ls)%2==0:
         x=2*rn
     else:
         x=(2*rn)/3
-    x=rn.as_integer_ratio()
-    out=[x[0],x[1]]
-    return out
+    if rn<0 or Ls[0]<x:
+        return [-1,-1]
+    # x=x.as_integer_ratio()
+    # out=[x[0],x[1]]
+    return fractioner(x)
+
+def fractioner(x):
+    counter=1
+    while x%1!=0:
+        counter*=10
+        x*=10
+    tuplx=(x,counter)
+    #print(tuplx)
+    gcd=gcder(tuplx)
+    return [int(tuplx[0]/gcd),int(tuplx[1]/gcd)]
+
+def gcder(tuplx):
+    x=tuplx[0]
+    y=tuplx[1]
+    small=min(x,y)
+    gcd=-1
+    for i in range(1,small+1):
+        if x%i==0 and y%i==0:
+            gcd=i
+    return gcd
