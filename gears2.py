b15f304e977dd6a515faf7919c84c15949f1e8d7
Michael High <74934387+michael-high@users.noreply.github.com>
2021-04-02 00:34:53 -0400

    Update gears2.py

--- gears2.py
+++ gears2.py
@@ -1,25 +1,34 @@
 def solution(pegs):
     Ls=[]
     for i in range(1,len(pegs)):
-        Ls.append(pegs[i]-pegs[i-1]) #generate Ls
-    print(Ls)
-    rn=Ls[0]
-    for i in range(1,len(pegs)-1):
+        Ls.append(pegs[i]-pegs[i-1]) #generate Ls, L1=a+b, L2=b+c, etc
+    #print(Ls)
+    rn=0                         #0 + L1
+    for i in range(0,len(Ls)):  #Alternate + -
         if i%2==0:
-            rn=rn+Ls[i]
+            rn=rn+Ls[i]       #Because we index starting with 0, and L1, L2, L3...Ln are starting at 1, evens and odds are flipped from algebra
         else:
             rn=rn-Ls[i]
-    print(rn)
-    if len(Ls)%2==0:  #a-e=rn
+    #print(rn)
+    if len(Ls)%2==0:  #a-e=L1-L2+L3....
         x=2*rn
     else:
-        x=(2*rn)/3  #a+e=rn
-    # y=rn
-    # if len(Ls)%2==1:
-    #     y=rn/3
-    print(x)
-    if rn<0 or Ls[0]<rn or Ls[-1]<rn: #if radius no worky, return [-1,-1]
+        x=(2*rn)/3  #a+e=
+    
+    #print(x)
+    y=rn
+    if len(Ls)%2==1:
+        y=rn/3
+
+    radii=[x]
+    for i in range(0,len(Ls)):  #a=x,   b=L1-x,  c=L2-b
+        radii.append(Ls[i]-radii[i])
+        if i+1<len(Ls)-1:
+            if radii[i+1]>Ls[i+1]:
+                return [-1,-1]
+    print(radii)
+    if x<1 or Ls[0]<x or Ls[-1]<x: #if radius no worky, return [-1,-1]
         return [-1,-1]
-    if type(x)==float:
+    if x%1!=0:
         return [int(x*3),3]
-    return [x,1]
+    return [int(x),1]
