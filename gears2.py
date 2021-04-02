ae306feb054c1d784260befa4cf08015793f77a2
Michael High <74934387+michael-high@users.noreply.github.com>
2021-04-01 23:45:22 -0400

    Update gears2.py

--- gears2.py
+++ gears2.py
@@ -1,41 +1,25 @@
-ef solution(pegs):
+def solution(pegs):
     Ls=[]
     for i in range(1,len(pegs)):
-        Ls.append(pegs[i]-pegs[i-1])
-    #print(Ls)
+        Ls.append(pegs[i]-pegs[i-1]) #generate Ls
+    print(Ls)
     rn=Ls[0]
     for i in range(1,len(pegs)-1):
         if i%2==0:
             rn=rn+Ls[i]
         else:
             rn=rn-Ls[i]
-    #print(rn)
-    if len(Ls)%2==0:
+    print(rn)
+    if len(Ls)%2==0:  #a-e=rn
         x=2*rn
     else:
-        x=(2*rn)/3
-    if rn<0 or Ls[0]<x:
+        x=(2*rn)/3  #a+e=rn
+    # y=rn
+    # if len(Ls)%2==1:
+    #     y=rn/3
+    print(x)
+    if rn<0 or Ls[0]<rn or Ls[-1]<rn: #if radius no worky, return [-1,-1]
         return [-1,-1]
-    # x=x.as_integer_ratio()
-    # out=[x[0],x[1]]
-    return fractioner(x)
-
-def fractioner(x):
-    counter=1
-    while x%1!=0:
-        counter*=10
-        x*=10
-    tuplx=(x,counter)
-    #print(tuplx)
-    gcd=gcder(tuplx)
-    return [int(tuplx[0]/gcd),int(tuplx[1]/gcd)]
-
-def gcder(tuplx):
-    x=tuplx[0]
-    y=tuplx[1]
-    small=min(x,y)
-    gcd=-1
-    for i in range(1,small+1):
-        if x%i==0 and y%i==0:
-            gcd=i
-    return gcd
+    if type(x)==float:
+        return [int(x*3),3]
+    return [x,1]
